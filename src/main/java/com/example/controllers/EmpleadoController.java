bdab87badef3c75aaca75130bcee51f88fb30b04
vrma <vrmachado@gmail.com>
2026-06-18 10:32:01 +0200

    creando formulario de alta

--- src/main/java/com/example/controllers/EmpleadoController.java
+++ src/main/java/com/example/controllers/EmpleadoController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.entities.Empleado;
+import com.example.services.DepartamentoService;
 import com.example.services.EmpleadoService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,13 +17,32 @@ import lombok.RequiredArgsConstructor;
 public class EmpleadoController {
 	
 	private final EmpleadoService empleadoService;
+	private final DepartamentoService departamentoService;
 
 	@GetMapping("/listar")
 	public String listarEmpleados(Model model) {
 		
 		model.addAttribute("empleados",
-				empleadoService.getAllEmpleados()); // Agrega la lista de empleados al modelo
+				empleadoService.getAllEmpleados()); 
+		// Agrega la lista de empleados al modelo
 		
 		return "listadoEmpleados"; // Retorna el nombre de la vista para listar empleados
+	}
+	
+	// Método para mostrar el formulario de creación de empleado
+	@GetMapping("/alta")
+	public String mostrarFormularioAlta(Model model) {
+		
+		// Se necesitan los departamentos desde la capa de servicios
+		model.addAttribute("departamentos", 
+				departamentoService.getAllDepartamentos());
+		
+		// Se necesita enviar un objeto Empleado vacio, para que se vinculen
+		// sus propiedades con cada control (elemento input, select, etc) 
+		// del formulario
+		model.addAttribute("empleado",
+				new Empleado());
+		
+		return "formularioAltaModificacion";
 	}
 }
